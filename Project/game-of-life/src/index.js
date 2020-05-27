86f73ac6522fba44d99f09340be8d0f5df6639f6
Dave Betts <ibedave777@gmail.com>
2020-05-27 15:09:26 -0400

    grid created and clickable

--- Project/game-of-life/src/index.js
+++ Project/game-of-life/src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Grid extends React.Component {
+class Box extends React.Component {
+  
+  selectBox = () => {
+    this.props.selectBox(this.props.row, this.props.col)
+  }
+  
   render() {
-    const width = this.props.cols * 14;
+    return (
+      <div className={this.props.boxClass} id={this.props.id} onClick={this.selectBox}/>
+    )
+  }
+}
+
+class Grid extends React.Component {
+  
+  render() {
+    const width = (this.props.cols * 16) + 1;
     var rowsArr = [];
     var boxClass = "";
     for (var i = 0; i < this.props.rows; i++) {
@@ -18,14 +32,15 @@ class Grid extends React.Component {
             key={boxID}
             row={i}
             col={j}
-            selectBox={}
-        )
+            selectBox={this.props.selectBox}
+            />
+        );
       }
     }
 
     return (
       <div className="grid" style={{width: width}}>
-        {{rowsArr}}
+        {rowsArr}
       
       </div>
     )
@@ -44,6 +59,14 @@ class Container extends React.Component {
       }
     }
 
+    selectBox = (row, col) => {
+      let gridCopy = arrayClone(this.state.gridFull);
+      gridCopy[row][col] = !gridCopy[row][col];
+      this.setState({
+        gridFull: gridCopy
+      })
+    }
+
     render() {
         return (
             <div>
@@ -52,11 +75,16 @@ class Container extends React.Component {
                   gridFull={this.state.gridFull}
                   rows={this.rows}
                   cols={this.cols}
+                  selectBox={this.selectBox}
                 />
                 <h2>Generations: {this.state.generation}</h2>    
             </div>
         );
     }
+}
+
+function arrayClone(arr) {
+  return JSON.parse(JSON.stringify(arr));
 }
 
 ReactDOM.render(<Container />, document.getElementById('root'));
